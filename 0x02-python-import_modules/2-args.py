50c4c7348570b16837ad21210b82750c4aefa697
samuel sholaye <samuelsho659@gmail.com>
2022-06-30 14:40:02 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def principal():
-    print('{} argument'.format(len(argv) - 1), end='')
-    if len(argv) == 1:
-         print('s.')
-    elif len(argv) == 2:
-        print(':')
-    else:
-         print('s:')
-    for i in range(1, len(argv)):
-        print('{}: {}'.format(i, argv[i]))
-
-
 if __name__ == "__main__":
-      principal()
+     import sys
+     len = len(sys.argv) - 1
+     print("{} argument{}{}"
+             .format(len, "" if len == 1 else "s", "." if len == 0 else ":"))
+     i = 1
+     while i <= len:
+          print("{}: {}".format(i, sys.argv[i]))
+          i += 1
