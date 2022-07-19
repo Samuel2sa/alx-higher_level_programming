b4b99eeee36408795145fed4ed8036021f97bee7
samuel sholaye <samuelsho659@gmail.com>
2022-07-19 12:35:38 -0700

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,6 +13,5 @@ class Square:
             size (int): size of a side of the square
         Returns:
             None                                                                        """                                                                             if type(size) is not int:             
-            raise TypeError("size must be an integer")                                  else:                                                                               if size < 0:                                                                        raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+            raise TypeError("size must be an integer")  
+
