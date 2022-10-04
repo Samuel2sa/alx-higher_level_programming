5a3907d026a21775d2ce3528d46d6cccc696474a
samuel sholaye <samuelsho659.com>
2022-10-04 19:06:24 +0000

    chm

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,33 +1,16 @@
 #!/usr/bin/node
-
 // module.exports = class Rectangle {
-
 //   constructor (w, h) {
-
 // this.width = w;
-
 // this.height = h;
-
 //   }
-
 // };
 
-
-
 // OR
-
 class Rectangle {
-
   constructor (w, h) {
-
     this.width = w;
-
     this.height = h;
-
   }
-
 }
-
-
-
 module.exports = Rectangle;
