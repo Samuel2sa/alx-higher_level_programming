4e5913f1f03da7cd44b9c91e5b351b0f9a73077e
samuel sholaye <samuelsho659.com>
2022-10-04 21:03:18 +0000

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
@@ -9,9 +9,11 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      const temp = [];
-      for (let j = 0; j < this.width; j++) temp.push('X');
-      console.log(`${temp.join('')}`);
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
+      }
+      console.log(s);
     }
   }
 }
